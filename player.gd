bebb4a867d36ff2349707ff09d534cd9ea9806a8
Miłosz Skrzypiec <skrzypiec.business@gmail.com>
2024-11-08 16:24:40 +0100

    Update style
    [Godot style guide](https://docs.godotengine.org/en/stable/tutorials/scripting/gdscript/gdscript_styleguide.html) suggests camel_case for naming variables

--- player.gd
+++ player.gd
@@ -6,11 +6,11 @@ extends CharacterBody2D
 func _physics_process(delta: float) -> void:
 	
 	# Get movement input from player
-	# -1 <= xDirection, yDirection <= 1
-	var xDirection: float = Input.get_axis("left", "right")
-	var yDirection: float = Input.get_axis("up", "down")
+	# -1 <= x_direction, ydirection <= 1
+	var x_direction: float = Input.get_axis("left", "right")
+	var y_direction: float = Input.get_axis("up", "down")
 	
-	velocity.x = lerp(velocity.x,speed * xDirection,acceleration * delta)
-	velocity.y = lerp(velocity.y,speed * yDirection,acceleration * delta)
+	velocity.x = lerp(velocity.x,speed * x_direction,acceleration * delta)
+	velocity.y = lerp(velocity.y,speed * y_direction,acceleration * delta)
 
 	move_and_slide()
