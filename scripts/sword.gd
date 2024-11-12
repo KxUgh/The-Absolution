8d09a84d15486c2ce3627d25af80c20d2004737d
Miłosz Skrzypiec <skrzypiec.business@gmail.com>
2024-11-12 22:23:23 +0100

    Post-merge fixes

--- scripts/sword.gd
+++ scripts/sword.gd
@@ -11,18 +11,14 @@ func attack(origin_position: Vector2, target_position: Vector2) -> void:
 	since_last_attack = 0
 	var angle: float = origin_position.angle_to_point(target_position)
 	var slash_instance: Attack = slash.instantiate()
+	slash_instance.collision_layer = weapon_owner.collision_layer
+	slash_instance.collision_mask = weapon_owner.collision_mask
 	slash_instance.rotation = angle
+	slash_instance.damage = damage
 	
 	if target_position.x - origin_position.x < 0:
 		slash_instance.sprite.play("Slash_Left")
 	else:
 		slash_instance.sprite.play("Slash_Right")
+		
 	add_child(slash_instance)
-
-func init_slash(angle: float) -> Attack:
-	var slash_instance: Attack = slash.instantiate()
-	slash_instance.collision_layer = weapon_owner.collision_layer
-	slash_instance.collision_mask = weapon_owner.collision_mask
-	slash_instance.rotation = angle
-	slash_instance.damage = damage
-	return slash_instance
