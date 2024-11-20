a1a494e79f55f64637c7dff558dafec060f1fd6b
Neti-was-taken-so-im-Neti01 <tomekbaranowski@vp.pl>
2024-11-20 22:50:31 +0100

    Blight and balance
    Balanced blight
    Modified starting player damage

--- scripts/player_data.gd
+++ scripts/player_data.gd
@@ -7,6 +7,6 @@ enum BuffType{
 
 @export var max_health: float = 10
 @export var speed: float = 100
-@export var damage: float = 2
+@export var damage: float = 3
 @export var health: float
 @export var buff: BuffType
